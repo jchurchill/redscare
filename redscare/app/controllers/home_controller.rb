1b370cf0763bc81991bf86ab2c5736b91fc41e49
Justin Churchill <justin.e.churchill@gmail.com>
2016-02-14 12:12:44 -0500

    Require login to hit home_controller (our only page right now)

--- redscare/app/controllers/home_controller.rb
+++ redscare/app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
+  # All methods on this controller require logged-in user;
+  # if hitting otherwise, redirect to login page
+  before_filter :authenticate_user!
+
   def index
   end
 end
