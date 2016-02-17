a6bd3193c190f77261793b9c40a177699f59f602
Justin Churchill <justin.e.churchill@gmail.com>
2016-02-16 23:36:54 -0500

    Fixing incorrect comment

--- redscare/app/models/nomination_vote.rb
+++ redscare/app/models/nomination_vote.rb
@@ -2,7 +2,7 @@ class NominationVote < ActiveRecord::Base
 
   # ===== Schema =====
   # :nomination (:nomination_id) => Nomination
-  # :creator (:creator_id) => User
+  # :user (:user_id) => User
   # :upvote => boolean
   # :created_at => datetime
   # :updated_at => datetime
