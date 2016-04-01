4c7ba1d04072b591ddf8e7f697e36a859a620499
Justin Churchill <justin.e.churchill@gmail.com>
2016-04-01 00:39:27 -0400

    Implemented inactive selection section

--- redscare/client/app/bundles/GameRoom/components/phases/nomination/InactiveSelectionSection.jsx
+++ redscare/client/app/bundles/GameRoom/components/phases/nomination/InactiveSelectionSection.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
-import PlayerList from '../../PlayerList.jsx';
+import classNames from 'classnames';
+import css from '../../PlayerList.scss'
 
 import Nomination from 'lib/game/nominationHelper';
 import User from 'lib/game/userHelper';
@@ -14,8 +15,34 @@ class InactiveSelectionSection extends React.Component {
     super(props, context);
   }
 
+  isPlayerNominated(userId) {
+    const { nomination, currentUser } = this.props;
+    return nomination.nominees.some(p => p.id === userId);
+  }
+
+  remainingNominations() {
+    const { nomination: { nominees, requiredNomineeCount } } = this.props;
+    return requiredNomineeCount - nominees.length;
+  }
+
+  renderPlayer(p) {
+    const isPlayerNominated = this.isPlayerNominated(p.id);
+    const className = classNames(
+      css.player,
+      isPlayerNominated ? css.nominatedPlayer : ''
+    );
+    return (<div key={p.id} className={className}>{p.name}</div>);
+  }
+
   render() {
-    return <div>InactiveSelectionSection</div>;
+    const { nomination: { playerProvider: { players } } } = this.props;
+    const stillNominating = this.remainingNominations() > 0;
+    return (
+      <div>
+        <div>{ stillNominating ? "The round leader is nominating." : "Nomination complete." }</div>
+        <div>{ players.map(p => this.renderPlayer(p)) }</div>
+      </div>
+    );
   }
 }
 
