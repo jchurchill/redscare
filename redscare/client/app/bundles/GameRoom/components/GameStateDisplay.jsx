3f69c7cf6e39d38db54191be95dc0649d7535cfe
Justin Churchill <justin.e.churchill@gmail.com>
2016-04-10 17:42:01 -0400

    css modules for game state display

--- redscare/client/app/bundles/GameRoom/components/GameStateDisplay.jsx
+++ redscare/client/app/bundles/GameRoom/components/GameStateDisplay.jsx
@@ -1,4 +1,6 @@
 import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+import css from './GameStateDisplay.scss'
 import GameRoundDisplay from './GameRoundDisplay';
 import Game from 'lib/game/gameHelper';
 import Round from 'lib/game/roundHelper';
@@ -16,7 +18,32 @@ class GameStateDisplay extends React.Component {
     const { game } = this.props
     // dictionary: (round_number) => round info
     const existingRounds = game.rounds.reduce((rs, r) => { rs[r.roundNumber] = r; return rs; }, {})
-    return [1,2,3,4,5].map((i) => ({ roundNumber: i, info: existingRounds[i] }));
+    return [1,2,3,4,5].map((i) => ({
+      roundNumber: i,
+      info: existingRounds[i],
+      classnames: this._getRoundClassnames(existingRounds[i])
+    }));
+  }
+
+  _getRoundClassnames(roundInfo) {
+    const cssClasses = [css.roundMarker];
+    if (!roundInfo) {
+      cssClasses.push(css.unstarted);
+    }
+    else {
+      switch (roundInfo.outcome) {
+        case Round.outcomes.SUCCESS:
+          cssClasses.push(css.success);
+          break;
+        case Round.outcomes.FAILURE:
+        case Round.outcomes.OUT_OF_NOMINATIONS:
+          cssClasses.push(css.failure);
+          break;
+        default:
+          cssClasses.push(css.active);
+      }
+    }
+    return classnames(cssClasses);
   }
 
   render() {
@@ -26,27 +53,11 @@ class GameStateDisplay extends React.Component {
       return <div></div>;
     }
     return (
-      <div style={{ display: 'inline-block' }}>
+      <div>
         <div>
           {
             rounds.map((r) => 
-              <div key={r.roundNumber} style={(()=>{
-                const sharedStyle = { display: 'inline-block', margin: '5px', padding: '5px', border: '1px solid black', borderRadius: '3px' }
-                if (!r.info) { return { ...sharedStyle, color: 'silver' } }
-                let moreStyle;
-                switch (r.info.outcome) {
-                  case Round.outcomes.SUCCESS:
-                    moreStyle = { color: 'blue', backgroundColor: 'cyan' }
-                    break;
-                  case Round.outcomes.FAILURE:
-                  case Round.outcomes.OUT_OF_NOMINATIONS:
-                    moreStyle = { color: 'red', backgroundColor: 'pink' }
-                    break;
-                  default:
-                    moreStyle = { border: '3px solid black' }
-                }
-                return { ...sharedStyle, ...moreStyle }
-              })()}>
+              <div key={r.roundNumber} className={r.classnames} >
                 Round {r.roundNumber}
               </div>
             )
