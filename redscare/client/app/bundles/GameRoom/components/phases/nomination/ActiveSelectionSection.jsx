319f2e86eecfadc3d8be0aba025b55eabef70274
Justin Churchill <justin.e.churchill@gmail.com>
2016-05-01 12:23:36 -0400

    Improved nomination (active selection) UI

--- redscare/client/app/bundles/GameRoom/components/phases/nomination/ActiveSelectionSection.jsx
+++ redscare/client/app/bundles/GameRoom/components/phases/nomination/ActiveSelectionSection.jsx
@@ -2,7 +2,8 @@ import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import css from './SelectionSection.scss'
 
-import PlayerIcon from '../../PlayerIcon'
+import PlayerIcon from '../../PlayerIcon.jsx'
+import Collapse from 'react-collapse';
 
 import Nomination from 'lib/game/nominationHelper';
 import User from 'lib/game/userHelper';
@@ -33,27 +34,67 @@ class ActiveSelectionSection extends React.Component {
     this.props.nominate(player.id);
   }
 
-  renderPlayer(p) {
-    const isPlayerNominated = this.isPlayerNominated(p.id);
-    const remainingNominations = this.remainingNominations();
-    const additionalClass = isPlayerNominated ? css.nominated : '';
-    const disabled = isPlayerNominated || remainingNominations === 0;
-    const buttonText = isPlayerNominated ? "Selected" : "Nominate"
-    return (
-      <div key={p.id} className={cx(css.potentialNominee, additionalClass)}>
-        <PlayerIcon player={p} />
-        <button onClick={this.nominate.bind(this, p)} disabled={disabled}>{buttonText}</button>
-      </div>
-    );
-  }
-
   render() {
     const { nomination: { playerProvider: { players } } } = this.props;
     const stillNominating = this.remainingNominations() > 0;
     return (
       <div>
         <div>{ stillNominating ? "Choose players to nominate." : "Nomination complete." }</div>
-        <div>{ players.map(p => this.renderPlayer(p)) }</div>
+        <div>
+          { players.map(p =>
+            <div key={p.id} className={css.inline} style={{ verticalAlign: 'top' }}>
+              <NominationParticipant
+                player={p}
+                selected={this.isPlayerNominated(p.id)}
+                disabled={this.remainingNominations() === 0}
+                onSelect={() => this.nominate(p)}
+              />
+            </div>
+          ) }
+        </div>
+      </div>
+    );
+  }
+}
+
+class NominationParticipant extends React.Component {
+  static propTypes = {
+    player: PropTypes.instanceOf(User).isRequired,
+    selected: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onSelect: PropTypes.func
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { staged: false }
+  }
+
+  toggleStaged() {
+    // only allow staging when not selected or disabled
+    const { selected, disabled } = this.props;
+    if (selected || disabled) { return; }
+    const { staged } = this.state;
+    this.setState({ staged: !staged });
+  }
+
+  confirmed() {
+    const { selected, disabled, onSelect } = this.props;
+    if (onSelect) { onSelect(); }
+    this.setState({ staged: false });
+  }
+
+  render() {
+    const { player, selected, disabled } = this.props;
+    const { staged } = this.state; 
+    return (
+      <div className={css.participant}>
+        <div onClick={() => this.toggleStaged()} className={selected ? css.selected : disabled ? css.disabled : css.selectable}>
+          <PlayerIcon player={player} />
+        </div>
+        <Collapse isOpened={staged} keepCollapsedContent={true} springConfig={{stiffness: 200, damping: 20}}>
+          <div onClick={() => this.confirmed()} className={css.nominate}>Nominate</div>
+        </Collapse>
       </div>
     );
   }
